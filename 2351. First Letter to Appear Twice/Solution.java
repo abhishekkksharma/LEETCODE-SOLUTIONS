0d4330c7110c225773798f11227850388b97c017
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-09-08 08:58:04 +0530

    2351. First Letter to Appear Twice

---
+++ 2351. First Letter to Appear Twice/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int[] freq = new int[26]; 
+        
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            freq[idx]++;
+            if (freq[idx] == 2) {
+                return s.charAt(i); 
+            }
+        }
+        return ' '; 
+    }
+}
