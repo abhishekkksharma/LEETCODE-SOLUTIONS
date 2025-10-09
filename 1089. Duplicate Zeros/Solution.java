0cd8de2eef820987737cd5fe5794daabcc4c7e2f
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-10-09 10:29:13 +0530

    1089. Duplicate Zeros

---
+++ 1089. Duplicate Zeros/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                al.add(0);
+            }
+            al.add(arr[i]);
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=al.get(i);
+        }
+    }
+}
