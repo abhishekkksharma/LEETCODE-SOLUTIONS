34dccb42fc2de6f1e7ebb22e7d12b907462d8c54
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-10-13 08:41:53 +0530

    2942. Find Words Containing Character

---
+++ 2942. Find Words Containing Character/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int i=0;i<words.length;i++){
+            if(words[i].indexOf(x)!=-1) a.add(i);
+        }
+        return a;
+    }
+}
