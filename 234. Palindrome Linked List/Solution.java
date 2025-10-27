68f321de095c0986b554d66bcec717320e560225
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-10-27 10:08:50 +0530

    optimal solution

--- 234. Palindrome Linked List/Solution.java
+++ 234. Palindrome Linked List/Solution.java
@@ -1,21 +1,31 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode current = head;
-        ArrayList<Integer> nums = new ArrayList<>();
-        while (current != null) {
-            nums.add(current.val);
-            current = current.next;
+        // ListNode current = head;
+        // ArrayList<Integer> nums = new ArrayList<>();
+        // while (current != null) {
+        //     nums.add(current.val);
+        //     current = current.next;
+        // }
+        // return palindromeCheck(nums);
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast = fast.next.next;
         }
-        return palindromeCheck(nums);
-    }
-
-    private boolean palindromeCheck(ArrayList<Integer> nums) {
-        int left = 0;
-        int right = nums.size() - 1;
-        while (left < right) {
-            if (!nums.get(left).equals(nums.get(right))) return false;
-            left++;
-            right--;
+        ListNode prev=null , pres=slow;
+        ListNode next=pres.next;
+        while(pres!=null){
+            pres.next=prev;
+            prev=pres;
+            pres=next;
+            if(next!=null) next=next.next;
+        }
+        ListNode first =head, second = prev;
+        while(second!=null){
+            if(first.val!=second.val) return false;
+            first=first.next;
+            second=second.next;
         }
         return true;
     }
