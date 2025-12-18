f18a9de0d9af70031da0820f5b13d507e4b0b7c8
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-12-18 10:02:13 +0530

    884. Uncommon Words from Two Sentences

---
+++ 884. Uncommon Words from Two Sentences/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+        String s = s1 + " " + s2;
+        Map<String, Integer> wordCount = new HashMap<>();
+        String[] words = s.split(" ");
+
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                result.add(entry.getKey());
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
