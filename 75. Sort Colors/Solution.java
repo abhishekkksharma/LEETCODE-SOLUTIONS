bc23169d0165ac850d57ae26b7ebc30ee5bbe12c
abhishekkksharma <abhisheksharma7340733@gmail.com>
2026-01-16 10:21:18 +0530

    75. Sort Colors

--- 75. Sort Colors/Solution.java
+++ 75. Sort Colors/Solution.java
@@ -1,5 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int start=0;
+        int last=nums.length-1;
+        int i=0;
+        while( i<=last){
+            if(nums[i]==0){
+                swap(nums,start,i);
+                start++;
+                i++;
+            }else if(nums[i]==2){
+                swap(nums,last,i);
+                last--;
+            }else{
+                i++;
+            }
+        }
+    }
+    private void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
