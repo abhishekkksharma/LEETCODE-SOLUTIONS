354e83db40b373a2e72e4bae03acbcfa7567cb49
abhishekkksharma <abhisheksharma7340733@gmail.com>
2026-01-27 08:57:36 +0530

    98. Validate Binary Search Tree

---
+++ 98. Validate Binary Search Tree/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public boolean isValidBST(TreeNode root) {
+        inOrder(root);
+        for(int i=1;i<list.size();i++){
+            if(list.get(i)<=list.get(i-1)) return false;
+        }
+        return true;
+    }
+    private void inOrder(TreeNode root){
+        if(root==null) return;
+        inOrder(root.left);
+        list.add(root.val);
+        inOrder(root.right);
+    }
+}
