622603849f6017999a08b8cfc2119be4ae3a4d4b
abhishekkksharma <abhisheksharma7340733@gmail.com>
2026-04-20 08:58:32 +0530

    2078. Two Furthest Houses With Different Colors

---
+++ 2078. Two Furthest Houses With Different Colors/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxDistance(int[] colors) {
+        int n=colors.length;
+        int max=0;
+        for(int i=0;i<n;i++){
+            if(colors[i]!=colors[0]){
+                max=Math.max(max,i);
+            }
+            if(colors[i]!=colors[n-1]){
+                max=Math.max(max,n-i-1);
+            }
+        }
+        return max;
+    }
+}
