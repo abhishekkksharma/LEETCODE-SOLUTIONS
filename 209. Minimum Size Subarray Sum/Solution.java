95512860ed0896f508e570e4b271ee0d2a6447fb
abhishekkksharma <abhisheksharma7340733@gmail.com>
2026-07-07 09:56:43 +0530

    209. Minimum Size Subarray Sum

---
+++ 209. Minimum Size Subarray Sum/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left=0, right=0, min=Integer.MAX_VALUE;
+        int sum=0;
+
+        while(right<nums.length){
+            sum+=nums[right];
+            while(sum>=target){
+                min=Math.min(min,right-left+1);
+                sum-=nums[left++];
+            }
+            right++;
+        }
+        return min==Integer.MAX_VALUE ? 0:min;
+    }
+}
