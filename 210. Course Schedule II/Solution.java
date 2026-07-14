1435da8d25b2bdccb8d61f545df00c81755ad4cf
abhishekkksharma <abhisheksharma7340733@gmail.com>
2026-07-14 09:47:54 +0530

    210. Course Schedule II

---
+++ 210. Course Schedule II/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
+        ArrayList<Integer> topo=new ArrayList<>();
+        int[] indegree=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+            indegree[v]++;
+            adj.get(u).add(v);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.offer(i);
+        }
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            topo.add(curr);
+            for(int i:adj.get(curr)){
+                indegree[i]--;
+                if(indegree[i]==0) q.offer(i);
+            }
+        }
+        Collections.reverse(topo);
+        int[] order=new int[topo.size()];
+        for(int i=0;i<topo.size();i++){
+            order[i]=topo.get(i);
+        }
+        return topo.size()==numCourses?order:new int[]{};
+    }
+}
