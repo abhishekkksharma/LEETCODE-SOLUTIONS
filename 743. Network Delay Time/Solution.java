cb0e4e7fd6488c932a6247d3fd3260368f5b2090
abhishekkksharma <abhisheksharma7340733@gmail.com>
2026-07-23 09:47:37 +0530

    743. Network Delay Time

---
+++ 743. Network Delay Time/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int[] dist=new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k]=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<times.length;j++){
+                int u=times[j][0];
+                int v=times[j][1];
+                int wt=times[j][2];
+                if(dist[u]!=Integer.MAX_VALUE && dist[u]+wt<dist[v]){
+                    dist[v]=dist[u]+wt;
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1;
+            result = Math.max(result, dist[i]);
+        }
+        return result;
+    }
+}
